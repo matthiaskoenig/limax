8fb52793af5573f3df9e329f9c8acdb8bf5bc59f
Matthias Koenig <konigmatt@googlemail.com>
2022-11-30 16:50:48 +0100

    updated limax metadata processing

--- src/limax/io.py
+++ src/limax/io.py
@@ -3,7 +3,10 @@ Reading data from RAW Limax files.
 
 Anonymization.
 """
+
+from pydantic import BaseModel
 from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 from typing import Any, Dict, List
 
@@ -11,14 +14,43 @@ import pandas as pd
 from pymetadata.log import console
 
 
-def read_metadata(str: str) -> None:
-    """Read LiMAx metadata."""
-    pass
+class MetaData(BaseModel):
+    """LiMAx metadata.
+
+    0 # mID 102
+    1 # 'doc' (, )
+    2 # Dr. Max Mustermann
+    3 # 01.01.2010 08:30
+    4 # utouARg
+    5 # 160 cm
+    6 # 70 kg
+    7 # 43,295187
+    8 # 44,395187
+    9 # 630,0
+    10 # Nahrungskarenz: über 3 Std., Raucher: Nein, Sauerstoff: Nein, Beatmung: Nein, Medikation: Ja
+    """
+
+    mid: str
+    name: str
+    datetime: str
+    height: float
+    weight: float
+    smoking: bool
+    oxygen: bool
+    ventilation: bool
+    medication: bool
+    food_abstinence: str
 
 
-def read_data(str: str) -> None:
-    """Read LiMAx data."""
-    pass
+class Limax(BaseModel):
+    """LiMAx DOB curve."""
+
+    metadata: MetaData
+    data: pd.DataFrame
+
+    class Config:
+        """Config for DOB curve."""
+        arbitrary_types_allowed = True
 
 
 def read_limax_dir(input_dir: Path, output_dir: Path) -> None:
@@ -32,7 +64,7 @@ def read_limax_dir(input_dir: Path, output_dir: Path) -> None:
 
 def read_limax_file(
     limax_csv: Path, output_path: Path, line_offset: int = 13
-) -> pd.DataFrame:
+) -> Limax:
     """Read limax data."""
     console.log(f"Processing '{limax_csv}' -> '{output_path}'")
     with open(limax_csv, "r") as f:
@@ -42,6 +74,24 @@ def read_limax_file(
         lines = [line for line in lines if len(line) > 0]
         # strip header lines
         time, dob, error = [], [], []
+
+        # parse metadata
+        md_lines = lines[:line_offset]
+        metadata_dict: Dict[str, Any] = {
+            "mid": ,
+            "name": md_lines[],
+            "datetime": "str",
+            "height": "float",
+            "weight": "float",
+            "smoking": "bool",
+            "oxygen": "bool",
+            "ventilation": "bool",
+            "medication": "bool",
+            "food_abstinence": "str",
+        }
+        metadata: MetaData = MetaData(**metadata_dict)
+
+        # parse data
         for line in lines[line_offset:]:
             tokens = [t.strip() for t in line.split("\t")]
             time.append(int(tokens[0]))
@@ -63,7 +113,7 @@ def read_limax_file(
     df.sort_values(by=["time"], inplace=True)
     df.to_csv(output_path, sep="\t", index=False)
 
-    return df
+    return Limax(metadata=metadata, data=df)
 
 
 if __name__ == "__main__":
@@ -71,8 +121,8 @@ if __name__ == "__main__":
 
     df = read_limax_file(
         limax_csv=EXAMPLE_LIMAX_PATH,
-        output_path=PROCESSED_DIR / EXAMPLE_LIMAX_PATH.stem,
+        output_path=PROCESSED_DIR / EXAMPLE_LIMAX_PATH.name,
     )
     print(df)
 
-    read_limax_dir(input_dir=RAW_DIR, output_dir=PROCESSED_DIR)
+    # read_limax_dir(input_dir=RAW_DIR, output_dir=PROCESSED_DIR)
